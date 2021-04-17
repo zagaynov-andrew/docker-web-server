4d3e3ddb19c90ae67ed1f451c08b1614e9fab16a
Zagaynov Andrew <zagaynov.drew@gmail.com>
2021-04-17 22:58:05 +0300

    phpMyAdmin

--- Dockerfile
+++ Dockerfile
@@ -6,16 +6,23 @@
 #    By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/13 14:19:50 by ngamora           #+#    #+#              #
-#    Updated: 2021/04/17 17:45:42 by ngamora          ###   ########.fr        #
+#    Updated: 2021/04/17 21:39:36 by ngamora          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FROM debian:buster
 
 RUN apt-get update && \
+    apt-get install -y apt-utils && \
     apt-get install -y vim && \
     apt-get install -y mariadb-server && \
-    apt-get install -y nginx
+    apt-get install -y nginx && \
+    apt-get install -y wget && \
+    apt-get install -y php php-fpm php-mysql php-xml
+RUN wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-english.tar.gz && \
+    tar -zxvf phpMyAdmin-5.1.0-english.tar.gz && \
+    rm phpMyAdmin-5.1.0-english.tar.gz
+RUN service php7.3-fpm start
 
 COPY ./srcs/default /etc
 
--- srcs/start_server.sh
+++ srcs/start_server.sh
@@ -6,13 +6,15 @@
 #    By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/14 20:53:22 by ngamora           #+#    #+#              #
-#    Updated: 2021/04/17 17:58:56 by ngamora          ###   ########.fr        #
+#    Updated: 2021/04/17 22:21:38 by ngamora          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # replacing the nginx config file
 rm /etc/nginx/sites-available/default
 mv etc/default etc/nginx/sites-available/default
+
+mv phpMyAdmin-5.1.0-english /var/www/html/phpMyAdmin
 
 # setting up rights
 chown -R www-data /var/www
@@ -25,13 +27,12 @@ echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_admin'@'localhost' \
         IDENTIFIED BY 'admin' WITH GRANT OPTION;" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
-echo "startting..."
-
 mkdir /var/www/html/data
 touch /var/www/html/data/file_1.txt
 touch /var/www/html/data/file_2.txt
 touch /var/www/html/data/file_3.txt
 
 service nginx restart
+service php7.3-fpm restart 
 
 bash
